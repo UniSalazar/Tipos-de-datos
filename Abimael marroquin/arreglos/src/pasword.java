519ace004a963ea6f4bc5a80a7e1469cb87538a4
Abimael <abimaelmaza@gmail.com>
2023-07-13 12:59:48 -0500

    caracteres

--- Abimael marroquin/arreglos/src/pasword.java
+++ Abimael marroquin/arreglos/src/pasword.java
@@ -8,6 +8,11 @@ public class pasword {
 		  String password = JOptionPane.showInputDialog(null, "Ingrese la contraseña:");
 		  String letraObjetivoInput = JOptionPane.showInputDialog(null, "Ingrese la letra objetivo:");
 		  char letraObjetivo = letraObjetivoInput.charAt(0);
+	
+		// Convertir la contraseña a un arreglo de caracteres
+	        char[] arregloPassword = password.toCharArray();	
+		  
+		  
 	}
 
 }
