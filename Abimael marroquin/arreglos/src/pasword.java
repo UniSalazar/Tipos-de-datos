dab9dae27c6bbe57e0f137d842cdf82693334c75
Abimael <abimaelmaza@gmail.com>
2023-07-13 12:49:08 -0500

    aca inicio con importar la libreria

---
+++ Abimael marroquin/arreglos/src/pasword.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class pasword {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
--- src/Pasword.java
+++ src/Pasword.java
@@ -3,15 +3,30 @@ import javax.swing.JOptionPane;
 public class Pasword {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 // Obtener la contraseña del usuario utilizando JOptionPane
+        String password = JOptionPane.showInputDialog(null, "Ingrese la contraseña:");
 
-		String usuario = JOptionPane.showInputDialog("Ingrese su nombre de usuario:");
-		String contraseña = JOptionPane.showInputDialog("Ingrese su contraseña:");
-		// Verificar si el usuario y la contraseña son correctos
-        if (usuario.equals("abimael") && contraseña.equals("12345")) {
-            JOptionPane.showMessageDialog(null, "Inicio de sesión exitoso. ¡Bienvenido, " + usuario + "!");
-        } else {
-        	JOptionPane.showMessageDialog(null, "Usuario o contraseña incorrectos. Por favor, inténtelo nuevamente.");
+        // Obtener la letra objetivo utilizando JOptionPane
+        String letraObjetivoInput = JOptionPane.showInputDialog(null, "Ingrese la letra objetivo:");
+        char letraObjetivo = letraObjetivoInput.charAt(0);
+
+        // Convertir la contraseña a un arreglo de caracteres
+        char[] arregloPassword = password.toCharArray();
+
+        // Verificar si la letra objetivo está presente en el arreglo
+        boolean letraEncontrada = false;
+        for (char A : arregloPassword) {
+            if (A== letraObjetivo) {
+                letraEncontrada = true;
+                break;
+            }
         }
-	}
+
+        // Mostrar el resultado utilizando JOptionPane
+        if (letraEncontrada) {
+            JOptionPane.showMessageDialog(null, "La contraseña contiene la letra objetivo.");
+        } else {
+            JOptionPane.showMessageDialog(null, "La contraseña no contiene la letra objetivo.");
+        }
+    }
 }
